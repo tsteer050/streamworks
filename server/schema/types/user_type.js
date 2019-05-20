b7cbf28f351c9397b647ca03e27dbdee387cc019
koy <kosatun@aol.com>
2019-05-20 09:24:41 -0700

    branch2 merge again

--- server/schema/types/user_type.js
+++ server/schema/types/user_type.js
@@ -1,14 +1,8 @@
 const mongoose = require("mongoose");
 const graphql = require("graphql");
-<<<<<<< HEAD
-const { GraphQLObjectType, GraphQLString, GraphQLID } = graphql;
-const LibraryType = require('./library_type');
-const Library = mongoose.model('libraries');
-=======
 const { GraphQLObjectType, GraphQLString, GraphQLID, GraphQLBoolean } = graphql;
 const LibraryType = require("./library_type");
 const Library = mongoose.model("libraries");
->>>>>>> backend-auth
 
 const UserType = new GraphQLObjectType({
   name: "UserType",
@@ -16,8 +10,7 @@ const UserType = new GraphQLObjectType({
     _id: { type: GraphQLID },
     name: { type: GraphQLString },
     email: { type: GraphQLString },
-    password: { type: GraphQLString },
-<<<<<<< HEAD
+    password: { type: GraphQLString }
     // library: {
     //   type: LibraryType,
     //   resolve(parentValue) {
@@ -30,19 +23,3 @@ const UserType = new GraphQLObjectType({
 });
 
 module.exports = UserType;
-=======
-    library: {
-      type: LibraryType,
-      resolve(parentValue) {
-        return Library.findById(parentValue.library)
-          .then(library => library)
-          .catch(err => null);
-      }
-    },
-    token: { type: GraphQLString },
-    loggedIn: { type: GraphQLBoolean }
-  })
-});
-
-module.exports = UserType;
->>>>>>> backend-auth
