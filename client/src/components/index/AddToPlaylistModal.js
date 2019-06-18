55648c57545644b591101b500d2459413b4306f2
koy <kosatun@aol.com>
2019-06-18 13:45:59 -0700

    refactored search results to include menu to add to playlist

--- client/src/components/index/AddToPlaylistModal.js
+++ client/src/components/index/AddToPlaylistModal.js
@@ -1,15 +1,13 @@
-import React from 'react';
+import React from "react";
 import { FETCH_USER_LIBRARY } from "../../graphql/queries";
-import { ADD_PLAYLIST_SONG } from '../../graphql/mutations';
+import { ADD_PLAYLIST_SONG } from "../../graphql/mutations";
 import { Query, Mutation } from "react-apollo";
-import NewPlaylistNestedModal from '../NewPlaylistNestedModal';
+import NewPlaylistNestedModal from "../NewPlaylistNestedModal";
 import "./addsongtoplaylistmodal.css";
 import * as emptyPlaylistImage from "../../images/empty-playlist.png";
 const jwt = require("jsonwebtoken");
 
-
 class AddToPlaylistModal extends React.Component {
-
   constructor(props) {
     super(props);
     this.state = {
@@ -20,8 +18,6 @@ class AddToPlaylistModal extends React.Component {
     this.toggleModal = this.toggleModal.bind(this);
     this.addToNewList = this.addToNewList.bind(this);
   }
-
-
 
   componentDidMount() {
     let token = localStorage.getItem("auth-token");
@@ -44,7 +40,7 @@ class AddToPlaylistModal extends React.Component {
       }
     });
     let modal = document.getElementById(id);
-    modal.classList.remove('visible');
+    modal.classList.remove("visible");
   }
 
   handleClick(addPlaylistSong, playlist) {
@@ -56,7 +52,7 @@ class AddToPlaylistModal extends React.Component {
       }
     });
     let modal = document.getElementById(id);
-    modal.classList.remove('visible');
+    modal.classList.remove("visible");
   }
 
   render() {
@@ -64,21 +60,34 @@ class AddToPlaylistModal extends React.Component {
 
     if (this.state.user) {
       return (
-        <div id={id} className='add-song-to-playlist-modal'>
+        <div id={id} className="add-song-to-playlist-modal">
           <div className="playlist-song-modal-div">
-            <span
-              className="x-button playlist-x"
-              onClick={() => {
-                let id = this.props.song._id + "modal";
-                let modal = document.getElementById(id);
-                modal.classList.remove('visible');
-              }}
-            >
-              X
-            </span>
+            <div className="x-button-container">
+              <span
+                className="x-button playlist-x"
+                onClick={() => {
+                  let id = this.props.song._id + "modal";
+                  let modal = document.getElementById(id);
+                  modal.classList.remove("visible");
+                }}
+              >
+                X
+              </span>
+            </div>
             <h1>Add to Playlist</h1>
-            <button onClick={(e) => { e.preventDefault();  this.toggleModal();} } className="modal-new-playlist-button">NEW PLAYLIST</button>
-            <Query query={FETCH_USER_LIBRARY} variables={{ id: this.state.user.id }}>
+            <button
+              onClick={e => {
+                e.preventDefault();
+                this.toggleModal();
+              }}
+              className="modal-new-playlist-button"
+            >
+              NEW PLAYLIST
+            </button>
+            <Query
+              query={FETCH_USER_LIBRARY}
+              variables={{ id: this.state.user.id }}
+            >
               {({ loading, error, data, client }) => {
                 if (loading) return "Loading...";
                 if (error) return `Error! ${error.message}`;
@@ -88,13 +97,13 @@ class AddToPlaylistModal extends React.Component {
                   <Mutation mutation={ADD_PLAYLIST_SONG}>
                     {addPlaylistSong => {
                       return (
-
                         <div>
                           <ul className="playlists-list">
                             {playlists.map(playlist => {
                               let image;
                               let length = 0;
-                              if (playlist.songs.length) length = playlist.songs.length;
+                              if (playlist.songs.length)
+                                length = playlist.songs.length;
                               if (playlist.songs.length) {
                                 image = playlist.songs[0].album.album_art_url;
                               } else {
@@ -107,41 +116,56 @@ class AddToPlaylistModal extends React.Component {
                                 backgroundSize: "145px"
                               };
                               return (
-                                <li className="playlist-modal-item" onClick={() => this.handleClick(addPlaylistSong, playlist)}>
+                                <li
+                                  className="playlist-modal-item"
+                                  onClick={() =>
+                                    this.handleClick(addPlaylistSong, playlist)
+                                  }
+                                >
                                   <div
                                     className="playlist-image"
                                     style={sectionStyle}
                                   />
-                                  <p className="playlist-name">{playlist.title}</p>
-                                  <p className="playlist-length">{length} songs</p>
+                                  <p className="playlist-name">
+                                    {playlist.title}
+                                  </p>
+                                  <p className="playlist-length">
+                                    {length} songs
+                                  </p>
                                 </li>
-                              )
+                              );
                             })}
                           </ul>
-                          <div id={"new-playlist-nested-modal" + this.props.song._id} className="nested-modal">
-                            <NewPlaylistNestedModal addPlaylistSong={addPlaylistSong} addToNewList={this.addToNewList} toggleModal={this.toggleModal}/>
+                          <div
+                            id={
+                              "new-playlist-nested-modal" + this.props.song._id
+                            }
+                            className="nested-modal"
+                          >
+                            <NewPlaylistNestedModal
+                              addPlaylistSong={addPlaylistSong}
+                              addToNewList={this.addToNewList}
+                              toggleModal={this.toggleModal}
+                            />
                           </div>
                         </div>
-                      )
+                      );
                     }}
                   </Mutation>
-                )
+                );
               }}
             </Query>
           </div>
         </div>
-      )
+      );
     } else {
       return (
         <div id={id}>
           <h1>MODAL!</h1>
         </div>
-      )
+      );
     }
-
   }
-
 }
-
 
 export default AddToPlaylistModal;
--- client/src/components/index/SongIndex.js
+++ client/src/components/index/SongIndex.js
@@ -1,31 +1,41 @@
-import React, { Fragment } from 'react';
-import SongIndexItem from './SongIndexItem';
+import React, { Fragment } from "react";
+import SongIndexItem from "./SongIndexItem";
 
 class SongIndex extends React.Component {
-
   render() {
-    const { songs, onHover, offHover, toggleSong } = this.props;
+    const {
+      songs,
+      onHover,
+      offHover,
+      toggleSong,
+      setDefaultTrack
+    } = this.props;
 
     const mappedSongs = songs.map((song, idx) => {
-      if (idx === 0) this.props.setDefaultTrack(song._id);
+      if (idx === 0) setDefaultTrack(song._id);
 
       let songLength = null;
-      if ((song.length % 60) >= 10) {
-        songLength = `${Math.floor(parseInt(song.length) / 60)}:${song.length % 60}`;
+      if (song.length % 60 >= 10) {
+        songLength = `${Math.floor(parseInt(song.length) / 60)}:${song.length %
+          60}`;
       } else {
-        songLength = `${Math.floor(parseInt(song.length) / 60)}:0${song.length % 60}`;
+        songLength = `${Math.floor(parseInt(song.length) / 60)}:0${song.length %
+          60}`;
       }
       return (
         <Fragment>
-         <SongIndexItem song={song} idx={idx} onHover={onHover} offHover={offHover} toggleSong={toggleSong} songLength={songLength} />
+          <SongIndexItem
+            song={song}
+            idx={idx}
+            onHover={onHover}
+            offHover={offHover}
+            toggleSong={toggleSong}
+            songLength={songLength}
+          />
         </Fragment>
-      )
+      );
     });
-    return (
-      <ul className="songs-list">
-        {mappedSongs}
-      </ul>
-    )
+    return <ul className="songs-list">{mappedSongs}</ul>;
   }
 }
 
