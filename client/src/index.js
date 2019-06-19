cc0cb57e3cd4d712fbdf4c852a55a85447b18b0b
tsteer <trevor.piano@gmail.com>
2019-06-19 13:24:02 -0700

    Change localhost to /

--- client/src/index.js
+++ client/src/index.js
@@ -20,7 +20,7 @@ const cache = new InMemoryCache({
 });
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:5000/graphql",
+  uri: "/graphql",
   headers: {
     authorization: localStorage.getItem("auth-token")
   }
