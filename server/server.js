9503069b336aab5b29ed1b4c93077e969b2fb3e5
tsteer <trevor.piano@gmail.com>
2019-06-18 17:35:25 -0700

    Change frontend to client in server.js

--- server/server.js
+++ server/server.js
@@ -38,9 +38,9 @@ app.use(
 
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('frontend/build'));
+  app.use(express.static('client/build'));
   app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
